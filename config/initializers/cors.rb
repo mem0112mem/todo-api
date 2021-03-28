24b97ad01031e3a6eda8b759e04fa583c03008fe
mem <mem0112@hotmail.com>
2021-03-29 00:38:47 +0900

    接続元のポート変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001'
+    origins 'localhost:8080'
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :delete]
   end
 end
